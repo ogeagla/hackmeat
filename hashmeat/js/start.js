fb108af9ae57a1c2cced3eaaf242a8e702f38112
Octavian Geagla <ogeagla@gmail.com>
2012-12-09 13:54:26 -0500

    JS improvements/fixes

--- hashmeat/js/start.js
+++ hashmeat/js/start.js
@@ -4,11 +4,15 @@ $(function(){
 
     $(".nav").click(function(){
     	var sid = $(this).attr("title");
+    	window.location.hash = sid;
     	console.log(sid);
     });
 
     $(document).scroll(function(){
     	var sid = $("body").find(".current").attr("title");
+    	if(window.location.hash != sid) {
+    		 window.location.hash = sid;
+    	}
 		console.log(sid);
     });
 
